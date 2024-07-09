d3f1df1be9ea8c8a1589298bcbf3b5fb1b195325
Denis <mugambimuthuri0@gmail.com>
2024-07-10 02:29:38 +0300

    Updated routes.jsx to import react-router-dom, created a router with a single route, and rendered it to the DOM

--- front-end/src/routes.jsx
+++ front-end/src/routes.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import App from "./App";
 
-const routes = () => {
-  return (
-    <div>routes</div>
-  )
-}
 
-export default routes
+
+
+
+export  const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+); 
