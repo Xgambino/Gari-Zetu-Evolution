e2087b784704887ecef769456d13750358d28d04
Denis <mugambimuthuri0@gmail.com>
2024-07-10 03:27:17 +0300

    Added imports, variables, and state to App component; replaced App function with arrow function and updated JSX return value.

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Background from './components/Background';
 
-function App() {
+const App = () => {
+  let heroData = [
+    {text1:"Indulge",text2:"your passions"},
+    {text1:"Unlock",text2:"the power of"},
+    {text1:"Grow",text2:"your life"}
+
+  ]
+  const [heroCount,setHeroCount] = useState(2);
+  const [playStatus,setPlayStatus] = useState(false);
+
   return (
-    <div>App</div>
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount}/>
+      
+    </div>
   )
 }
 
